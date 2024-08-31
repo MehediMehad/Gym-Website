e692038c382826a7e2e44b9dc95fd573a4d8ec9d
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-08-31 11:26:22 +0600

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import BgImg from "../src/assets/bg.png"
 
+const bgStyle = {
+  backgroundImage: `url(${BgImg})`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundAttachment: "fixed",
+};
 
 const App = () => {
   return (
     <div className="overflow-x-hidden ">
-      <Navbar/>
+      <div style={bgStyle}>
+        <Navbar/>
+        <Hero/>
+      </div>
     </div>
   );
 };
