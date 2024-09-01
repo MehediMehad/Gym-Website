62d875c231ac73440a4603ff9672d0262bc79e77
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-09-01 11:08:03 +0600

    Banner Section

--- src/components/Equipments/Equipments.jsx
+++ src/components/Equipments/Equipments.jsx
@@ -33,7 +33,7 @@ const EquipmentData = [
 const Equipments = () => {
     return (
         <div>
-            <div className="container ">
+            <div className="container py-24">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 font-playfair">
                     <div className="space-y-6 p-6">
                         <h1 className="text-3xl md:text-4xl font-bold ">What we offer for you</h1>
@@ -46,7 +46,7 @@ const Equipments = () => {
                                 variants={SlideLeft(item.delay)}
                                 initial="hidden"
                                 whileInView="visible"
-                                key={item.id} className="space-y-6 p-6 bg-[#fbfbfb] hover:bg-white rounded-xl hover:shadow-[0_0_22px_0_rgba(0,0,0,0.15)]">
+                                key={item.id} className="space-y-6 p-6 bg-[#f3f4f6] hover:bg-white rounded-xl hover:shadow-[0_0_22px_0_rgba(0,0,0,0.15)]">
                                     <div className="text-4xl">{item.icon}</div>
                                     <p className="text-2xl font-semibold">{item.title}</p>
                                     <p className="text-gray-500">{item.desc}</p>
